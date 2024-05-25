0540a3f64c12bd944dfabda7481c4fe162a33820
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-05-25 13:02:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Bot in Godez Community"];
+const statusMessages = ["Bot in Godez Community","https://shopswgcenter.shop/"];
 
 
 let currentIndex = 0;
